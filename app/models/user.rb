e56951318ed6d55f92633ae9c90366af1f6fbf9f
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-02-27 22:35:55 -0400

    css to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :bookings, dependent: :destroy
   has_many :cars, through: :bookings
-  # has_one_or_many :cars, dependent: :destroy
+  has_many :cars, dependent: :destroy
+  has_one_attached :avatar
 end
