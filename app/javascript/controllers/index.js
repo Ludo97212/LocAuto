52940beb898b53f0facecac026b088802f56514c
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-03-08 21:58:13 -0400

    modif searchbar

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,15 @@
 // import Flatpickr
 import Flatpickr from 'stimulus-flatpickr'
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 // Import style for flatpickr
 require("flatpickr/dist/flatpickr.css")
 
 
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
 
 
 // Manually register Flatpickr as a stimulus controller
--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -13,10 +13,12 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      // style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb"
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+
     this.map.addControl(new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
       mapboxgl: mapboxgl
