fdb8918988b10ee97d0e7add14532eb81ca4fe53
Ludovic Nourel <ludovic.nourel@gmail.com>
2022-03-23 15:34:31 -0300

    modif marker on show map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -16,4 +16,21 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   }
 }
 
+
+const mapShowElement = document.getElementById('map-show');
+if (mapShowElement) {
+  const map = new GMaps({ zoom: 2, el: '#map-show', lat: 0, lng: 0 });
+  const markers = JSON.parse(mapShowElement.dataset.markers);
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(2);
+  } else {
+    map.fitLatLngBounds(markers);
+    map.setZoom(14);
+  }
+}
+
 autocomplete();
